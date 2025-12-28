760459436ffd4f12f7b30e7428e61b84b89300b0
Omar <108215915+ExyoOmar@users.noreply.github.com>
2025-12-28 16:49:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('active');
+      observer.unobserve(entry.target);
+    }
+  });
+},{threshold:0.2});
+
+reveals.forEach(r=>observer.observe(r));
